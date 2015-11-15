7612836f84a0a54930034a80a0eb6639e4b9cb03
Marcus Gregersen <mgmano@gmail.com>
2015-11-15 18:01:14 +0100

    Diveded newsletter into two buckets

--- src/app/user/user.detail.nyhedsbrev.controller.js
+++ src/app/user/user.detail.nyhedsbrev.controller.js
@@ -8,13 +8,22 @@
   /** @ngInject */
   function UserDetailNyhedsbrevController($scope, $stateParams, mdbAPI) {
     var vm = this;
+    vm.scope = $scope;
 
     activate();
+
+    $scope.in_user_nyhedsbreve = function(value, index, array) {
+      return vm.scope.$parent.user.nyhedsbreve.indexOf(value.nyhedsbrev_id) > -1;
+    };
+
+    $scope.not_in_user_nyhedsbreve = function(value, index, array) {
+      return vm.scope.$parent.user.nyhedsbreve.indexOf(value.nyhedsbrev_id) === -1;
+    };
 
     function getNyhedsbreve() {
       mdbAPI.getNyhedsbreve().then(function(nyhedsbreve) {
         $scope.nyhedsbreve = nyhedsbreve;
-      })
+      });
     }
 
     function activate() {
