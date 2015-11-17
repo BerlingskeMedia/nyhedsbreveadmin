82f7046c78cdb5f1b3c414e264363918172be874
Marcus Gregersen <mgmano@gmail.com>
2015-11-17 14:03:54 +0100

    Smartlinkbuilder

--- src/app/components/smartlinkFormPost/smartlinkFormPost.directive.js
+++ src/app/components/smartlinkFormPost/smartlinkFormPost.directive.js
@@ -9,6 +9,10 @@
     var directive = {
       restrict: 'E',
       templateUrl: 'app/components/smartlinkFormPost/smartlinkFormPost.html',
+      link: function(scope, element, attrs) {
+        console.log(element);
+        scope.todohtml = element.outerHtml;
+      }
     };
     return directive;
   }
--- src/app/smartlinkbuilder/smartlinkbuilder.controller.js
+++ src/app/smartlinkbuilder/smartlinkbuilder.controller.js
@@ -6,9 +6,11 @@
     .controller('SmartlinkBuilderController', SmartlinkBuilderController);
 
   /** @ngInject */
-  function SmartlinkBuilderController($scope, $q, moment, mdbAPI,  nyhedsbreveadminConfig) {
+  function SmartlinkBuilderController($scope, $q, $sanitize, $templateCache, $compile, $interpolate, $timeout, $element, moment, mdbAPI,  nyhedsbreveadminConfig) {
     var vm = this;
     var SMARTLINK_BASEURL = nyhedsbreveadminConfig.SMARTLINK_BASEURL;
+
+    var formPostTemplate = $templateCache.get('formPostTemplate.html');
 
     activate();
 
@@ -24,12 +26,38 @@
       });
     }
 
+
+
+    function compilePostForm() {
+      // console.log(angular.element(compiled[0])[0].innerHTML);
+
+
+      // var interpolated = $interpolate(formPostTemplate)($scope);
+      // $scope.postSource = angular.element(compiled[0]);
+      var compiled = $compile(formPostTemplate)($scope);
+        console.log(compiled);
+
+      $timeout(function(){
+        //the code which needs to run after dom rendering
+
+              $scope.postSource = compiled[0].innerHTML;
+      }, 2000);
+
+
+    }
+
     function activate() {
+      $scope.POSTURL = SMARTLINK_BASEURL;
       vm.updateLocation = updateLocation;
       getData();
       debugLocation();
       setWatchers();
       setDefaults();
+    }
+
+    function doCompile() {
+      compilePostForm();
+      compileSmartlink();
     }
 
     function setDefaults() {
@@ -40,6 +68,7 @@
       $scope.action = 'subscribe';
       $scope.customerfield = 'email';
       $scope.startdate = new Date();
+      $scope.POSTURL = SMARTLINK_BASEURL;
 
       $scope.minDate = new Date();
     }
@@ -47,7 +76,7 @@
     function setWatchers() {
       var to_watch = ['selectedNyhedsbreve', 'selectedInteresser', 'selectedPermissions', 'flow', 'action', 'customerfield', 'startdate', 'enddate', 'location', 'landingpage'];
       for (var i = 0; i < to_watch.length; i++) {
-        $scope.$watch(to_watch[i], compileSmartlink );
+        $scope.$watch(to_watch[i], doCompile );
       }
     }
 
