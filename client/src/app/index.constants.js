c86513f37582d018419fdbbdc42aae5e392a53ac
Marcus Gregersen <mgmano@gmail.com>
2015-11-18 15:46:24 +0100

    Ready for dist

--- client/src/app/index.constants.js
+++ client/src/app/index.constants.js
@@ -6,7 +6,7 @@
     .module('nyhedsbreveadmin')
     .constant('moment', moment)
     .constant("nyhedsbreveadminConfig", {
-      "APIBASEURL": "http://localhost:7999/backend/",
+      "APIBASEURL": "/backend/",
       "LOCATIONID": 1,
       "SMARTLINK_BASEURL": "http://profil.berlingskemedia.dk/smartlink"
     });
