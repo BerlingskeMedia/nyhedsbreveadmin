457efc664acb19eb64c7e0aefffe11a55a8475ce
Daniel Kokott <dako@berlingskemedia.dk>
2016-01-10 09:28:50 +0100

    switched to standard port 8000

--- server/server.js
+++ server/server.js
@@ -71,7 +71,7 @@ var server = new Hapi.Server({
   }
 });
 
-server.connection({ port: process.env.PORT ? process.env.PORT : 7999 });
+server.connection({ port: process.env.PORT ? process.env.PORT : 8000 });
 
 server.route({
   method: 'GET',
